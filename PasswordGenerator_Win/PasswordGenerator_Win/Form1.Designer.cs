484290e9f1eab1f68fabcfdab000b8b5b23b7036
Michael Koby <michael@mkoby.com>
2011-09-07 10:40:37 -0500

    Added Windows version

---
+++ PasswordGenerator_Win/PasswordGenerator_Win/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace PasswordGenerator_Win
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.PasswordLengthText = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.GeneratedPasswordText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Password Length: ";
+            // 
+            // PasswordLengthText
+            // 
+            this.PasswordLengthText.Location = new System.Drawing.Point(115, 13);
+            this.PasswordLengthText.MaxLength = 2;
+            this.PasswordLengthText.Name = "PasswordLengthText";
+            this.PasswordLengthText.Size = new System.Drawing.Size(58, 20);
+            this.PasswordLengthText.TabIndex = 1;
+            this.PasswordLengthText.Text = "14";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(180, 8);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(135, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Use Uppercase Letters";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckStateChanged += new System.EventHandler(this.CheckboxCheckStateChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(180, 31);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(135, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Use Lowercase Letters";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckStateChanged += new System.EventHandler(this.CheckboxCheckStateChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(321, 35);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(137, 17);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "Use Special Characters";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckStateChanged += new System.EventHandler(this.CheckboxCheckStateChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Checked = true;
+            this.checkBox4.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox4.Location = new System.Drawing.Point(321, 12);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(90, 17);
+            this.checkBox4.TabIndex = 4;
+            this.checkBox4.Text = "Use Numbers";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckStateChanged += new System.EventHandler(this.CheckboxCheckStateChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(321, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Generate Password";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Generated Password: ";
+            // 
+            // GeneratedPasswordText
+            // 
+            this.GeneratedPasswordText.Location = new System.Drawing.Point(131, 104);
+            this.GeneratedPasswordText.Name = "GeneratedPasswordText";
+            this.GeneratedPasswordText.Size = new System.Drawing.Size(322, 20);
+            this.GeneratedPasswordText.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 146);
+            this.Controls.Add(this.GeneratedPasswordText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.PasswordLengthText);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Password Generator (Windows)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox PasswordLengthText;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox GeneratedPasswordText;
+    }
+}
+
---
+++ PasswordGenerator_Win/PasswordGenerator_Win/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PasswordGenerator_Win
+{
+    public partial class Form1 : Form
+    {
+        private static bool useUppercase = true,
+                        useLowercase = true,
+                        useNumbers = true,
+                        useSpecialCharacters = true;
+        private PasswordGenerator.Generator gen;
+
+        private void SetPasswordLength(string newLength)
+        {
+            int newLengthValue = gen.PasswordLength;
+            bool isNumber = int.TryParse(newLength, out newLengthValue);
+
+            if (isNumber)
+                gen.PasswordLength = newLengthValue;
+            else
+            {
+                PasswordLengthText.Text = "14";
+            }
+        }
+
+        private void SetGeneratorOptions(CheckBox cb)
+        {
+            switch (cb.Text)
+            {
+                case "Use Uppercase Letters":
+                    gen.UseUpperCaseLetters = cb.Checked;
+                    break;
+                case "Use Lowercase Letters":
+                    gen.UseLowerCaseLetters = cb.Checked;
+                    break;
+                case "Use Numbers":
+                    gen.UseNumbers = cb.Checked;
+                    break;
+                case "Use Special Characters":
+                    gen.UseSpecialCharacters = cb.Checked;
+                    break;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+            gen = new PasswordGenerator.Generator(14, useUppercase, useLowercase,
+                                              useNumbers, useSpecialCharacters);
+        }
+
+        private void CheckboxCheckStateChanged(object sender, EventArgs e)
+        {
+            SetGeneratorOptions((CheckBox)sender);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetPasswordLength(PasswordLengthText.Text);
+            GeneratedPasswordText.Text = gen.GeneratePassword();
+        }
+    }
+}
